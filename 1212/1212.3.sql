df8e41a3ee5a3d470d6aa85019a2671d9b30e930
s18013 <s18013@std.it-college.ac.jp>
2018-12-20 14:25:58 +0900

    repaired

--- 1212/1212.3.sql
+++ 1212/1212.3.sql
@@ -1,1 +1,1 @@
-select last_name, hire_date, salary from employees where (hire_date >= '06-01-01' and hire_date <= '06-12-31') and (salary < 9000) and (commissions_pct = 0);
+select last_name, hire_date, salary from employees where (hire_date between '06-01-01' and '06-12-31') and (salary < 9000) and (commission_pct is null);
--- 1212/1212.4.sql
+++ 1212/1212.4.sql
@@ -1,1 +1,1 @@
-select last_name, hire_date, salary from employees where ((hire_date >= '06-01-01' and hire_date <= '06-12-31') and (salary > 9000)) or (Commissions_pct = 0);
+select last_name, hire_date, salary from employees where ((hire_date >= '06-01-01' and hire_date <= '06-12-31') and ((salary > 9000) or (Commissions_pct is null)));
